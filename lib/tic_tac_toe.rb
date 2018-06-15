3f5983e01e02271e95c682ebd69c7759116c73bd
Ishani <ishaniamin@gmail.com>
2018-06-15 00:31:09 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -2,7 +2,7 @@ class TicTacToe
   def initialize
     @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
   end
-  
+
   WIN_COMBINATIONS = [
     [0,1,2], # Top row
     [3,4,5],  # Middle row
@@ -13,7 +13,7 @@ class TicTacToe
     [0,4,8],
     [2,4,6]
   ]
-  
+
   def display_board
     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
     puts "-----------"
@@ -21,24 +21,24 @@ class TicTacToe
     puts "-----------"
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
-  
+
   def input_to_index(input)
     index = input.to_i - 1
     return index
   end
-  
+
   def move(index, character="X")
     @board[index] = character
   end
-  
+
   def position_taken?(index)
     !(@board[index].nil? || @board[index] == " ")
   end
-  
+
   def valid_move?(index)
     index.between?(0,8) && position_taken?(index) == false
   end
-  
+
   def turn
     character = current_player
     puts "Please enter 1-9:"
@@ -46,7 +46,7 @@ class TicTacToe
     index = input_to_index(input)
     if valid_move?(index)
       move(index, character)
-    else 
+    else
       until valid_move?(index)
         puts "Please enter 1-9:"
         input = gets
@@ -58,68 +58,68 @@ class TicTacToe
   end
 
   def turn_count
-    count = 0 
+    count = 0
     @board.each do |spot|
       if spot != " "
-        count += 1 
+        count += 1
       end
     end
     return count
   end
-  
+
   def current_player
-    if turn_count % 2 == 0 
+    if turn_count % 2 == 0
       return "X"
-    else 
+    else
       return "O"
     end
   end
-  
+
   def won?
     WIN_COMBINATIONS.each do |combo|
-      if @board[combo[0]] == "X" && @board[combo[1]] == "X" && @board[combo[2]] == "X" 
-        return combo 
-      elsif @board[combo[0]] == "O" && @board[combo[1]] == "O" && @board[combo[2]] == "O" 
-        return combo  
+      if @board[combo[0]] == "X" && @board[combo[1]] == "X" && @board[combo[2]] == "X"
+        return combo
+      elsif @board[combo[0]] == "O" && @board[combo[1]] == "O" && @board[combo[2]] == "O"
+        return combo
       end
-    end 
+    end
     return false
   end
 
   def full?
     @board.each do |spot|
       if spot == " "
-        return false 
+        return false
       end
     end
-    return true 
+    return true
   end
 
   def draw?
     if full? && !won?
-      return true 
+      return true
     else
-      return false 
-    end
-  end
-  
-  def over?
-    if won? || draw? || full?
-      return true 
-    else 
       return false
     end
   end
-  
+
+  def over?
+    if won? || draw? || full?
+      return true
+    else
+      return false
+    end
+  end
+
   def winner
     winning_combo = won?
     if winning_combo != false
       return @board[winning_combo[0]]
-    else 
-      return nil 
+    else
+      return nil
     end
   end
-  
+
   def play
     while !over?
       turn
